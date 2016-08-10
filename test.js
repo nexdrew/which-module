f187238b97c6273382ea96a2a2150ead1bc75419
nexdrew <andrew@npmjs.com>
2016-08-10 12:28:23 -0400

    test: remove invalid assertion for supports-color

--- test.js
+++ test.js
@@ -31,10 +31,10 @@ test('returns falsy for system module', (t) => {
 })
 
 test('returns falsy for non-required', (t) => {
+  // note that a module may export a boolean (e.g. supports-color)
   t.falsy(whichModule(t))
   t.falsy(whichModule({}))
   t.falsy(whichModule(function () {}))
-  t.falsy(whichModule(false))
   t.falsy(whichModule('foobar'))
   t.falsy(whichModule(String))
   t.falsy(whichModule())
